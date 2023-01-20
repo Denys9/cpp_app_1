063092f261671d102aa0fa08fd29e5c88b6abbf5
Denys9 <113623082+Denys9@users.noreply.github.com>
2023-01-20 23:40:34 +0200

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+using namespace std;
+
 int main()
 {
-    std::cout << "Коли вийде - закину :)"
+	std::cout << "Enter year->";
+	int year;
+	std::cin >> year;
+	
+	cout << "Days = " << 365+int(year % 4 ==0 || year % 100 == 0 || year % 400 == 0) << endl;
+	
+	
+
+
+	return 0;
 }
