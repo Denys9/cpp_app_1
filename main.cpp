ce947f1c6808413d9b38ceb19b8d3b64221d61d9
Denys9 <113623082+Denys9@users.noreply.github.com>
2023-01-20 17:28:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,5 @@
 #include <iostream>
 int main()
 {
-    
-        float width, height, area, length;
-        std::cout << "Enter the data to calculate the parallelepiped:\a";
-        std::cout << "Width ->\a ";
-        std:: cin >> width;
-        std::cout << "Height ->\a ";
-        std::cin >> height;
-        std::cout << "Long ->\a ";
-        std::cin >> length;
-        area = width * height * length;
-        std::cout << "volume -> " << area;
-        std::cout << std::endl;
-        
-    
-
-	return 0;
+    std::cout << "Коли вийде - закину :)"
 }
